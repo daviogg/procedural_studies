a6b85180049a9696d50a71a591ca26ed5a4c2b90
daviogg <daviogg@hotmail.it>
2018-10-19 08:05:59 +0200

    Spostati scripts asset

---
+++ Assets/Scripts/RoomsHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomsHandler : MonoBehaviour {
+	public GameObject test;
+	void Start () {
+		
+
+	}
+	
+	void Update () {
+		
+	}
+}
