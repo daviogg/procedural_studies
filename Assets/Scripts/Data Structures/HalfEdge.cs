18ec8b4bc37c29560a0deddb74ba5a4bb310d9c8
daviogg <daviogg0@gmail.com>
2018-10-31 23:21:53 +0100

    Setup data structures for Delaunay triangulation

---
+++ Assets/Scripts/Data Structures/HalfEdge.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfEdge {
+
+    //The vertex the edge points to
+    public Vertex v;
+
+    //The face this edge is a part of
+    public Triangle t;
+
+    //The next edge
+    public HalfEdge nextEdge;
+    //The previous
+    public HalfEdge prevEdge;
+    //The edge going in the opposite direction
+    public HalfEdge oppositeEdge;
+
+    public HalfEdge(Vertex v)
+    {
+        this.v = v;
+    }
+}
